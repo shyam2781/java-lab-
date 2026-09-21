b9b921b1c6c9464e359fcfa649901dd4eba2ca17
shyam2781 <shyamkasani4@gmail.com>
2026-09-21 15:38:26 +0530

    Add files via upload

---
+++ Arraysum3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Arraysum3 {
+    // Static
+    static int Sum(int arr[]) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Sum of array elements: " + Sum(arr));
+     }
+}
